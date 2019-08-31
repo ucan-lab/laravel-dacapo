e1637726180c48f91241fcf8f28b9e93e0a13402
ゆうきゃん <ucan.lab.ggl@gmail.com>
2019-09-01 02:51:52 +0900

    fix create_users_table email not nullable

--- tests/Storage/laravel_legacy_default/1970_01_01_000000_create_users_table.php
+++ tests/Storage/laravel_legacy_default/1970_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->nullable(true);
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable(true);
             $table->string('password');
             $table->rememberToken();
