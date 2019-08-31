47733f5bbe720a1f579b5c32222e862e00aaf2bf
ゆうきゃん <ucan.lab.ggl@gmail.com>
2019-09-01 02:52:40 +0900

    fix password_resets email index

--- tests/Storage/laravel_legacy_default/1970_01_01_000000_create_password_resets_table.php
+++ tests/Storage/laravel_legacy_default/1970_01_01_000000_create_password_resets_table.php
@@ -15,7 +15,7 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email');
+            $table->string('email')->index();
             $table->string('token');
             $table->timestamp('created_at')->nullable(true);
         });
