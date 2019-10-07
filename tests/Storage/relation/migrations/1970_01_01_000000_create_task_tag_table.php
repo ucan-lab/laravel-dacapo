50e368651e9da7b27f90d872fcfafb9ca25c482d
mpyw <ryosuke_i_628@yahoo.co.jp>
2019-10-07 18:35:24 +0900

    Test: Use foreign key name

---
+++ tests/Storage/relation/migrations/1970_01_01_000000_create_task_tag_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaskTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_tag', function (Blueprint $table) {
+            $table->unsignedBigInteger('task_id');
+            $table->unsignedBigInteger('tag_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_tag');
+    }
+}
