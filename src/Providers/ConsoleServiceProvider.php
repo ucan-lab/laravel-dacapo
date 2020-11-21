55ce73f096f75656cca539b6feaa9c64cd8f1c9c
ucan-lab <35098175+ucan-lab@users.noreply.github.com>
2020-11-21 15:09:32 +0900

    Destroy everything

--- src/Providers/ConsoleServiceProvider.php
+++ src/Providers/ConsoleServiceProvider.php
@@ -1,15 +1,9 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace UcanLab\LaravelDacapo\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use UcanLab\LaravelDacapo\Console\DacapoCommand;
-use UcanLab\LaravelDacapo\Console\DacapoInitCommand;
-use UcanLab\LaravelDacapo\Console\DacapoSeedCommand;
 use UcanLab\LaravelDacapo\Console\DacapoClearCommand;
-use UcanLab\LaravelDacapo\Console\DacapoFreshCommand;
-use UcanLab\LaravelDacapo\Console\DacapoModelsCommand;
-use UcanLab\LaravelDacapo\Console\DacapoUninstallCommand;
 
 /**
  * Class ConsoleServiceProvider.
@@ -38,31 +32,7 @@ class ConsoleServiceProvider extends ServiceProvider
     protected function registerCommands(): void
     {
         $this->app->singleton('command.ucan.dacapo.init', function () {
-            return new DacapoInitCommand();
-        });
-
-        $this->app->singleton('command.ucan.dacapo', function () {
-            return new DacapoCommand();
-        });
-
-        $this->app->singleton('command.ucan.dacapo.fresh', function () {
-            return new DacapoFreshCommand();
-        });
-
-        $this->app->singleton('command.ucan.dacapo.seed', function () {
-            return new DacapoSeedCommand();
-        });
-
-        $this->app->singleton('command.ucan.dacapo.models', function () {
-            return new DacapoModelsCommand();
-        });
-
-        $this->app->singleton('command.ucan.dacapo.clear', function () {
             return new DacapoClearCommand();
-        });
-
-        $this->app->singleton('command.ucan.dacapo.uninstall', function () {
-            return new DacapoUninstallCommand();
         });
 
         $this->commands($this->provides());
@@ -75,12 +45,6 @@ class ConsoleServiceProvider extends ServiceProvider
     {
         return [
             'command.ucan.dacapo.init',
-            'command.ucan.dacapo',
-            'command.ucan.dacapo.fresh',
-            'command.ucan.dacapo.seed',
-            'command.ucan.dacapo.models',
-            'command.ucan.dacapo.clear',
-            'command.ucan.dacapo.uninstall',
         ];
     }
 }
