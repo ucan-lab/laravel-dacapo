1762ea66d786e6b15b92df9a714757117155c2ad
ucan-lab <ucan.lab.ggl@gmail.com>
2021-01-02 02:31:29 +0900

    test DacapoCommand mysql & postgresql

---
+++ tests/Console/DacapoCommand/postgresql/foreign_keys_two_tables_pattern/migrations/1970_01_01_000000_create_customer_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->integer('id');
+            $table->decimal('price');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
---
+++ tests/Console/DacapoCommand/postgresql/foreign_keys_two_tables_pattern/migrations/1970_01_01_000000_create_product_order_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_order', function (Blueprint $table) {
+            $table->increments('no');
+            $table->integer('product_category');
+            $table->integer('product_id');
+            $table->integer('customer_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_order');
+    }
+}
