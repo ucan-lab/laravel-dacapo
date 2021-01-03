4c7cae8de8f42bbb0899718bf9dd35e320f0b8a2
ucan-lab <ucan.lab.ggl@gmail.com>
2021-01-04 03:51:35 +0900

    test Connection

---
+++ tests/Console/DacapoCommand/mysql/connection/migrations/1970_01_01_000000_create_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('custom-connection')->create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('custom-connection')->dropIfExists('users');
+    }
+}
