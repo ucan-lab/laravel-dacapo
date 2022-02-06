2bb6637d8bbc67ae168d2cd38003d6244e8990ef
ucan-lab <ucan.lab.ggl@gmail.com>
2022-02-07 01:09:34 +0900

    feat Change Schema Format: Specify null for column modifier with empty arguments

--- src/Dacapo/Domain/Schema/Column/ColumnModifier/AutoIncrementModifier.php
+++ src/Dacapo/Domain/Schema/Column/ColumnModifier/AutoIncrementModifier.php
@@ -4,13 +4,13 @@ namespace UcanLab\LaravelDacapo\Dacapo\Domain\Schema\Column\ColumnModifier;
 
 final class AutoIncrementModifier implements ColumnModifier
 {
-    private bool $value;
+    private ?bool $value;
 
     /**
      * AutoIncrementModifier constructor.
-     * @param bool $value
+     * @param ?bool $value
      */
-    public function __construct(bool $value)
+    public function __construct(?bool $value)
     {
         $this->value = $value;
     }
