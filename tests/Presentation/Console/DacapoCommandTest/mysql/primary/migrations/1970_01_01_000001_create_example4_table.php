4d636104f043711d82d60679384a45ea9411e51f
ucan-lab <ucan.lab.ggl@gmail.com>
2022-04-01 02:50:16 +0900

    feat Add primary modifier

---
+++ tests/Presentation/Console/DacapoCommandTest/mysql/primary/migrations/1970_01_01_000001_create_example4_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('example4', function (Blueprint $table) {
+            $table->string('id')->primary();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('example4');
+    }
+};
